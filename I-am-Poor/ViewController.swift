e9262389cde9954124c5e9e7733af6ac3b3b1dcf
teckenrod <treyeck2@gmail.com>
2023-01-06 09:27:31 -0600

    Initial Commit

---
+++ I-am-Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-am-Poor
+//
+//  Created by Trey Eckenrod on 1/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
